94e87e4e729bdc132cee16ef385d29cdb3f0572b
ukari <muromi.ukari@gmail.com>
2019-11-13 04:05:50 +0800

    update

--- speedbar-git-respect-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package
-  "speednar-git-respect"
-  "0.0.1"
-  "particular respect git repo in speedbar"
-  '((f "0.8.0")
-    (emacs "23.1")))
-
--- speedbar-git-respect.el
+++ speedbar-git-respect.el
@@ -1,22 +1,62 @@
+;;; speedbar-git-respect.el --- Particular respect git repo in speedbar -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 chendianbuji@gmail.com
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Version: 0.0.1
+;; Url: https://github.com/ukari/speedbar-git-respect
+;; Author: Muromi Ukari <chendianbuji@gmail.com>
+;; Package-Requires: ((f "0.8.0") (emacs "24.1") (cl-lib "0.0.1"))
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; This package will override the emacs builtin speedbar-file-lists function
+;; and change its behaviour when the directory is a git repo.
+;;
+;; The file list will show the following stuffs:
+;; 1. All directorys and files tracked by git
+;; 2. All directorys and files untracked by git but not matched in .gitignore
+;;
+;; The unicode filename or directory name won't display correct unless
+;; disable the git's octal utf8 display to respect unicode.
+;; To disable it, run `git config --global core.quotepath off`
+
 (require 'f)
 (require 'speedbar)
 (require 'vc-git)
+(require 'cl-lib)
 
-(setq origin-file-lists (symbol-function 'speedbar-file-lists))
+;;; Code:
 
-(defun vc-git-dir-p (directory)
+(defvar speedbar-git-respect--origin-file-lists (symbol-function 'speedbar-file-lists))
+
+(defun speedbar-git-respect--vc-git-dir-p (directory)
+  "Check if DIRECTORY a git repo."
   (eq 'Git (condition-case nil (vc-responsible-backend directory) (error nil))))
 
 (defun speedbar-file-lists (directory)
-  (setq origin-directory default-directory)
-  (setq default-directory directory)
-  (let ((result (if (vc-git-dir-p directory)                    
-                    (git-file-lists directory)
-                  (funcall origin-file-lists directory))))
-    (setq default-directory origin-directory)
-    result))
+  "Redefine Emacs `speedbar-file-lists' function.
+Create file lists for DIRECTORY."
+  (if (speedbar-git-respect--vc-git-dir-p directory)
+      (let ((origin-directory default-directory))
+        (setq default-directory directory)
+        (speedbar-git-respect--git-file-lists directory)
+        (setq default-directory origin-directory))
+    (funcall speedbar-git-respect--origin-file-lists directory)))
 
-(defun git-file-lists (directory)
+(defun speedbar-git-respect--git-file-lists (directory)
+  "Create file lists for DIRECTORY which is git repo."
   (setq directory (expand-file-name directory))
   ;; find the directory, either in the cache, or build it.
   (or (and (not dframe-power-click) ;; In powerclick mode, always rescan.
@@ -27,17 +67,17 @@
       ;; git ls-files --other --directory
       ;; untracked ignored
       ;; git ls-files --other --directory --ignored --exclude-standard
-      (let ((tracked-fd (file-list-process (vc-git--run-command-string nil "ls-files" "--directory")))
-            (untracked-fd (file-list-process (vc-git--run-command-string nil "ls-files" "--other" "--directory")))
-            (untracked-ignored (file-list-process (vc-git--run-command-string nil "ls-files" "--other" "--directory" "--ignored" "--exclude-standard")))
+      (let ((tracked-fd (speedbar-git-respect--file-list-process (vc-git--run-command-string nil "ls-files" "--directory")))
+            (untracked-fd (speedbar-git-respect--file-list-process (vc-git--run-command-string nil "ls-files" "--other" "--directory")))
+            (untracked-ignored (speedbar-git-respect--file-list-process (vc-git--run-command-string nil "ls-files" "--other" "--directory" "--ignored" "--exclude-standard")))
             (dirs nil)
             (files nil))
-        (setf dirs (remove-if (lambda (x) (member x (car untracked-ignored))) (car untracked-fd)))
-        (setf files (remove-if (lambda (x) (member x (cadr untracked-ignored))) (cadr untracked-fd)))        
+        (setf dirs (cl-remove-if (lambda (x) (member x (car untracked-ignored))) (car untracked-fd)))
+        (setf files (cl-remove-if (lambda (x) (member x (cadr untracked-ignored))) (cadr untracked-fd)))
         (setf dirs (delete-dups (append (car tracked-fd) dirs)))
         (setf files (delete-dups (append (cadr tracked-fd) files)))
-        (setf dirs (remove-if (lambda (x) (>= 0 (length x))) dirs))
-        (setf files (remove-if (lambda (x) (>= 0 (length x))) files))
+        (setf dirs (cl-remove-if (lambda (x) (>= 0 (length x))) dirs))
+        (setf files (cl-remove-if (lambda (x) (>= 0 (length x))) files))
         (setf dirs (sort dirs 'string>))
         (setf files (sort files 'string>))
 	(let ((nl (cons (nreverse dirs) (list (nreverse files))))
@@ -47,15 +87,18 @@
                   speedbar-directory-contents-alist))
 	  nl))))
 
-(defun file-directory-split (path)
+(defun speedbar-git-respect--file-directory-split (path)
+  "Split PATH with os path separator."
   (split-string path (f-path-separator)))
 
-(defun file-list-process (list)
+(defun speedbar-git-respect--file-list-process (list)
+  "Process git ls-files result to a list.
+LIST is a multiline string of pathnames."
   (setq list (split-string list "\n"))
   (let ((dirs nil)
         (files nil))
     (mapc (lambda (path)
-            (let* ((res (file-directory-split path))
+            (let* ((res (speedbar-git-respect--file-directory-split path))
                    (name (car res)))
               (if (> (length res) 1)
                   (setq dirs (cons name dirs))
@@ -63,4 +106,6 @@
           list)
     (list (delete-dups dirs) files)))
 
-(provice 'speedbar-git-respect)
+(provide 'speedbar-git-respect)
+
+;;; speedbar-git-respect.el ends here
